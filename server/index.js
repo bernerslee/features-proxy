dfd95dc87803c3e62518d538e96f2445670c05e1
Jessica <jessica.herrera82094@gmail.com>
2019-05-09 14:46:21 -0700

    added artillery tests and new relic agent

--- server/index.js
+++ server/index.js
@@ -3,12 +3,15 @@ require('newrelic');
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var cors = require('cors')
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/',express.static(__dirname + '/../client', {maxAge: 5000})); //sets maxAge to 5sec
+app.use('/:id', express.static(__dirname+'/../client', {maxAge: 5000}));
+
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
-app.use('/:id', express.static(__dirname+'/../client'));
-// app.use(express.static(__dirname+'/../client'));
 var port = 3000;
 
 app.listen(port, ()=>{
